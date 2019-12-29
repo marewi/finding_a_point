0c1750336aec9ea5b3786a90284602886ad14215
Marc Wittlinger <marc-wittlinger@gmx.de>
2019-12-29 18:36:45 +0000

    formating

--- parameters.py
+++ parameters.py
@@ -1,17 +1,11 @@
-# size of grid environment
-SIZE = 128
+######################################
+### learning parameters
 
 # this is per training data (picture) -> 50000 per pic
-EPISODES = 1000
+EPISODES = 1
 
 # how often is the agent allowed to try to reach the goal
 steps = 10000
-
-# punishing the agent for each step
-MOVE_PENALTY = 1
-
-# rewarding the agent when it reaches the goal
-GOAL_REWARD = steps/2
 
 # controls randomnes of the agent
 epsilon = 0.5
@@ -31,3 +25,16 @@ DISCOUNT = 0.95
 # which logos should be used
 # strategy_filter = xxx
 # is created as a terminal option at the moment
+
+
+######################################
+### environment parameters
+
+# size of grid environment
+SIZE = 128
+
+# punishing the agent for each step
+MOVE_PENALTY = 1
+
+# rewarding the agent when it reaches the goal
+GOAL_REWARD = steps/2
