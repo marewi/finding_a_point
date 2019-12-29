231d644b545705ba4129656fa6059dacb2e8a59f
Marc Wittlinger <marc-wittlinger@gmx.de>
2019-12-29 19:54:24 +0000

    testing

--- main.py
+++ main.py
@@ -29,14 +29,13 @@ def main(argv):
     # data filter
     data_filter = ''
     try:
-        opts, args = getopt.getopt(argv, "s")
+        opts, args = getopt.getopt(argv,"s")
         print(opts, args)
     except getopt.GetoptError:
         print(colored("option doesnt exist", 'red'))
         sys.exit(2)
-    if opts == [('-s', '')] and args == []:
-        raise Exception(
-            colored("options for strategy was set, but no arguments", 'red'))
+    if opts == [('-s', '')] and args == []: 
+        raise Exception(colored("options for strategy was set, but no arguments", 'red'))
     if opts == []:
         print(colored("no data_filter was set", 'red'))
     else:
@@ -50,7 +49,7 @@ def main(argv):
 
     ######################################
     # backup old logs
-    newpath = r'./oldlogs'
+    newpath = r'./oldlogs' 
     if not os.path.exists(newpath):
         os.makedirs(newpath)
     source = './logs/'
@@ -65,22 +64,19 @@ def main(argv):
     loc = "./data/table_3000_clustered.xlsx"
     goals = data_input(loc, data_filter)
 
-    print(
-        colored(f"--- time to create environment: {time.time()-env_time} ---", 'blue'))
+    print(colored(f"--- time to create environment: {time.time()-env_time} ---", 'blue'))
 
     ######################################
     # build model
     model_time = time.time()
     model = Model_table()
-    print(colored(
-        f"--- time to create q-table model: {time.time()-model_time} ---", 'blue'))
+    print(colored(f"--- time to create q-table model: {time.time()-model_time} ---", 'blue'))
 
     ######################################
     # train model
     learning_time = time.time()
     episode_rewards, q_table, epsilons = q_learning(goals, model)
-    print(
-        colored(f"--- time to train model: {time.time()-learning_time} ---", 'blue'))
+    print(colored(f"--- time to train model: {time.time()-learning_time} ---", 'blue'))
 
     ######################################
     # measure the results
@@ -96,7 +92,7 @@ def main(argv):
     # write_event(epsilons, "epsilon")
     plt1 = write_event(episode_rewards, 'rewards per episode')
     plt2 = write_event(accumulated_reward, 'accumulated reward')
-
+    
     with tf.Session() as sess:
         summary = sess.run(plt1)
         summary2 = sess.run(plt2)
@@ -105,7 +101,6 @@ def main(argv):
         writer.add_summary(summary2)
         writer.close()
     sess.close()
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy
 from model_table import Model_table
+import tensorflow as tf
 
 qtable = Model_table()
 
@@ -13,7 +14,13 @@ ax.set_xticks(numpy.arange(0, 1, 0.1))
 ax.set_yticks(numpy.arange(0, 1., 0.1))
 plt.scatter(x, y)
 plt.grid()
-plt.show()
+
+with tf.Session() as sess:
+    summary = sess.run(fig)
+    writer = tf.summary.FileWriter('./test')
+    writer.add_summary(summary)
+    writer.close()
+sess.close()
 
 ############
 
