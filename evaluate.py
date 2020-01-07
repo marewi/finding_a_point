c49db68092473a818f30ab9aa09ae0b27caac352
Marc Wittlinger <marc-wittlinger@gmx.de>
2020-01-07 21:52:34 +0000

    evaluation finsihed maybe

--- evaluate.py
+++ evaluate.py
@@ -53,13 +53,16 @@ def main(argv):
     tries = []
     for test_nr in range(len(test_goals)):
         # find state with max Q value
-            x_of_state_with_max_q, y_of_state_with_max_q = max(q_table.items(), key=operator.itemgetter(1))[0]
-            eva_agent = Agent(x_of_state_with_max_q, y_of_state_with_max_q)
-            print("agent created")
+        x_of_state_with_max_q, y_of_state_with_max_q = max(q_table.items(), key=operator.itemgetter(1))[0]
+        eva_agent = Agent(x_of_state_with_max_q, y_of_state_with_max_q)
+        print("agent created")
         for i in range(10000):
             state = (eva_agent.x, eva_agent.y)
-            action = np.argmax(q_table[state])  # get action
+            print(f"state: {state}")
+            action = np.argmax(q_table[state]) # get action
+            print(f"action: {action}")
             eva_agent.action(action)  # take the action
+            # print(f"eva_agent: {eva_agent}")
             if eva_agent.x == test_goals[test_nr].x and eva_agent.y == test_goals[test_nr].y:
                 tries.append(i)
                 break # reached goal
